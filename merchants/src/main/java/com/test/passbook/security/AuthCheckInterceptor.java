fef6df513ee8352c3bbc98ee5a326dcc7463ea0a
kevinZ <zhengkaiw851@gmail.com>
2021-03-20 22:25:44 +0800

    add constant security etc

---
+++ merchants/src/main/java/com/test/passbook/security/AuthCheckInterceptor.java
@@ -0,0 +1,10 @@
+package com.test.passbook.security;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+/**
+ * <h1>权限拦截器</h1>
+ */
+public class AuthCheckInterceptor implements HandlerInterceptor {
+
+}
