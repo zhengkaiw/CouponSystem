a7522b9f289bb6118373bdb6244080c2320ade75
kevinZ <zhengkaiw851@gmail.com>
2021-03-20 22:53:34 +0800

    create dao

---
+++ merchants/src/main/java/com/test/passbook/dao/MerchantsDao.java
@@ -0,0 +1,23 @@
+package com.test.passbook.dao;
+
+import com.test.passbook.entity.Merchants;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * <h1>Merchants Dao 接口</h1>
+ */
+public interface MerchantsDao extends JpaRepository<Merchants, Integer> {
+    /**
+     * <h2>根据 id 获取商户对象</h2>
+     * @param id 商户 id
+     * @return {@link Merchants}
+     */
+    Merchants findById(int id);
+
+    /**
+     * <h2>根据商户名称获取商户对象</h2>
+     * @param name 商户名称
+     * @return {@link Merchants}
+     */
+    Merchants findByName(String name);
+}
