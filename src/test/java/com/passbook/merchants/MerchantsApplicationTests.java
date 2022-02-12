fe3e648e3233723688ed466b16b5828ba7557926
kevinZ <zhengkaiw851@gmail.com>
2022-02-12 17:09:06 +0800

    initiate the project

---
+++ src/test/java/com/passbook/merchants/MerchantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passbook.merchants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
