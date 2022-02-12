d53f787260663d9014c7e556fffd0a89198f7d25
kevinZ <zhengkaiw851@gmail.com>
2022-02-13 00:40:25 +0800

    initiate passbook

---
+++ passbook/src/test/java/com/passbook/passbook/PassbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passbook.passbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
